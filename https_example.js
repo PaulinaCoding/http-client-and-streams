1def29e17c67932f3d05ac5128bf34cdb708af3a
PaulinaCoding <paulina.pietrykiewicz@gmail.com>
2018-06-11 05:36:43 +0000

    Chunk option added

--- https_example.js
+++ https_example.js
@@ -11,6 +11,11 @@ var options = {
 var callback = function(response) {
   console.log('In response handler callback!');
   console.log('Response: ', response);
+///This was added as next step after the response (step 6 in compass)
+  response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
 }
 //Initial version of the callback:
 // var callback = function() {
